8b84d55e4ecfb168aea21770e4c810db9dc2930e
Ashish5028 <114331767+Ashish5028@users.noreply.github.com>
2024-01-22 17:47:33 +0530

    product

--- src/product/components/productPage.jsx
+++ src/product/components/productPage.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "./product.css";
+import { productData } from "./data";
+import ProductItem from "../productItem";
+import { Header } from "../../component/Header";
+import { FooterPage } from "../../component/footerPage";
 export default function ProdctPage() {
   return (
-    <div>
-      {/* <Header /> */}
-      <div className="items-center justify-items-center">
-        <span>Product</span>
-      </div>
-      <div className=" bg-zinc-100 shadow-lg p-3 ">
-        <div className=" flex  space-x-4 px-10 ">
-          <span className="text-sm  cursor-pointer">Flavour</span>
-          <span className="text-sm  cursor-pointer">Catogery</span>
-          <span className="text-sm  cursor-pointer">Dessert</span>
-        </div>
-      </div>
-    </div>
+    <>
+      <Header />
+      <br />
+      <br />
+      <main>
+        {productData.map((item) => (
+          <ProductItem key={item.id} item={item} />
+        ))}
+      </main>
+      <FooterPage />
+    </>
   );
 }
