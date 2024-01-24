f723c4ade90ec454d67489e7a6feca2de6ec54ee
Abhishek232110 <abhi7376862392@gmail.com>
2024-01-24 16:37:38 +0530

    add

--- src/product/index.jsx
+++ src/product/index.jsx
@@ -1,9 +1,11 @@
+import { DemoPage } from "./components/demo.jsx";
 import ProductPage from "./components/productPage.jsx";
 
 export default function ProductIndex() {
   return (
     <>
-      <ProductPage />
+      {/* <ProductPage /> */}
+      <DemoPage />
     </>
   );
 }
