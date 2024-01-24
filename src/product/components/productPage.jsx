97b7363c955ab395006bdfa1cb1a6593f1b9fa2f
sushil <sushil88081@gmail.com>
2024-01-24 16:26:04 +0530

    change in ui

--- src/product/components/productPage.jsx
+++ src/product/components/productPage.jsx
@@ -4,18 +4,10 @@ import { productData } from "./data";
 import ProductItem from "../productItem";
 import { Header } from "../../component/Header";
 import { FooterPage } from "../../component/footerPage";
-<<<<<<< HEAD
-import Card from "../../component/card";
-=======
->>>>>>> cdec71bb79293d9b6ba75a4fc3c8a95b2c9ebb6d
 import HeaderIndex from "../../header";
 export default function ProdctPage() {
   return (
     <>
-<<<<<<< HEAD
-=======
-      <HeaderIndex />
->>>>>>> cdec71bb79293d9b6ba75a4fc3c8a95b2c9ebb6d
       <br />
       <br />
       <main>
