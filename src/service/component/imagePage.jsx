ee16d4a8345d047acc0bd80d977e8ee7accb095a
Abhishek232110 <abhi7376862392@gmail.com>
2024-01-25 21:47:37 +0530

    ADD SUNM

---
+++ src/service/component/imagePage.jsx
@@ -0,0 +1,21 @@
+import cake from "../../assets/baker.png";
+import service from "../../assets/service-2.jpg";
+import down from "../../assets/down.jpg";
+import about from "../../assets/about.jpg";
+export default function ImagePage() {
+  return (
+    <>
+      <div className=" relative h-full mx-4 ">
+        <div className=" absolute left-10 top-10 h-72 w-56 rounded-md ">
+          <img src={down} alt="" className="rounded-md" />
+        </div>
+        <div className=" m-32 ml-auto absolute inset-0  rounded-md h-72 w-56 ">
+          <img src={about} alt="" className="rounded-md" />
+        </div>
+        <div className="  absolute bottom-10 right-0 h-72 w-56 ">
+          <img src={service} alt="" className="rounded-md" />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/service/component/testApi.jsx
+++ src/service/component/testApi.jsx
@@ -1,42 +1,79 @@
-import { useState, useEffect } from "react";
 import { FaTshirt, FaCartPlus } from "react-icons/fa";
 import { BsCake2 } from "react-icons/bs";
-import { TbTruckDelivery } from "react-icons/tb";
-
-import Card from "../../component/card";
+import { CiDeliveryTruck } from "react-icons/ci";
+import "./app.css";
+import ImagePage from "./imagePage";
 const TestApi = () => {
   return (
-    <div className="flex  h-screen px-20">
-      <div className=" w-1/2 bg-cyan-300 pl-10">
-        <h1 className="text-bgColor">//OUR SERVICE</h1>
-        <br />
-        <p className="text-4xl">What Do We Offer For You ?</p>
-        <br />
-        <p>
+    <div className="flex  h-screen px-14 ">
+      <div className=" w-1/2  ">
+        <h1 className="text-bgColor text-lg py-4 pl-12">//OUR SERVICE</h1>
+        <p className="text-4xl font-semibold font-serif px-10">
+          What Do We Offer For You ?
+        </p>
+        <p className=" py-4 px-10">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          <br />
-          exercitationem culpa debitis eligendi aliquid consectetur,
-          <br />
-          placeat in officia nemo hic?
+          exercitationem culpa debitis eligendi aliquid consectetur
         </p>
-        <div className="flex justify-around ">
-          <FaTshirt className="h-10 w-10 bg-yellow-500 text-white rounded-full border p-2" />
-          <h1 className="">Quality Products</h1>
-          <BsCake2 className="h-10 w-10 bg-yellow-500 text-white rounded-full border p-2" />
-          <h1>Custom Products</h1>
+        <div className="flex px-10 justify-between my-5">
+          <div>
+            <div className="flex space-x-3 relative  items-center  text-yellow-300 pr-6 cursor-pointer">
+              <FaTshirt
+                size="40px"
+                className=" text-white bg-yellow-500 rounded-full p-2 items-center justify-center"
+              />
+              <h1 className="">Quality Products</h1>
+            </div>
+            <p className="py-5">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit
+            </p>
+          </div>
+          <div>
+            <div className="flex space-x-3 relative  items-center  text-yellow-300 pr-6 cursor-pointer">
+              <BsCake2
+                size="40px"
+                className="text-white bg-yellow-500 rounded-full p-2 items-center justify-center"
+              />
+              <h1>Custom Products</h1>
+            </div>
+            <p className="py-5">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit
+            </p>
+          </div>
         </div>
-        <div className=" grid grid-cols-2 gap-4 place-content-around ">
-          <FaCartPlus className="h-10 w-10 bg-yellow-500 text-white rounded-full border p-2" />
-          <TbTruckDelivery className="h-10 w-10 bg-yellow-500 text-white rounded-full border p-2" />
+        <div className="flex justify-between px-10 my-5">
+          <div>
+            <div className=" flex space-x-3 relative  items-center   pr-6 cursor-pointer">
+              <FaCartPlus
+                size="40px"
+                className="text-white bg-yellow-500 rounded-full p-2 items-center justify-center"
+              />
+              <p className="text-headingColor text-lg">Online Order</p>
+            </div>
+            <p className="py-5">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit
+            </p>
+          </div>
+          <div>
+            <div className="flex space-x-3 relative  items-center  text-yellow-300 pr-12 cursor-pointer">
+              <CiDeliveryTruck
+                size="40px"
+                className="text-white bg-yellow-500 rounded-full p-2 items-center justify-center"
+              />
+              <p>Home Delivery</p>
+            </div>
+            <p className="py-5">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit
+            </p>
+          </div>
         </div>
+        {/* <div className="Product flex">
+          <CiDeliveryTruck size="35px" className="" />
+          <p>Home Delivery</p>
+        </div> */}
       </div>
-      <div className="w-1/2 bg-yellow-100">
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe quidem
-          quae ipsum debitis facilis hic vel dignissimos veritatis voluptatem.
-          Odit optio vitae, veniam eius cumque corrupti dolore quidem magni
-          sapiente!
-        </p>
+      <div className="w-1/2 ">
+        <ImagePage />
       </div>
     </div>
   );
