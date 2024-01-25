5fd55b2afc3a9c1040cecdad5792c514cf3a0b8a
Abhishek232110 <abhi7376862392@gmail.com>
2024-01-25 22:28:58 +0530

    add

--- src/service/component/imagePage.jsx
+++ src/service/component/imagePage.jsx
@@ -5,13 +5,13 @@ export default function ImagePage() {
   return (
     <>
       <div className=" relative h-full mx-10  ">
-        <div className=" absolute left-40 top-20 h-72 w-56 bg-yellow-600 rounded-md ">
-          {/* <img src={down} alt="" className="rounded-md" /> */}
-        </div>
-        <div className=" m-10  absolute inset-0  rounded-md h-72 w-56 ">
+        <div className=" absolute left-40 top-28 h-72 w-56  rounded-md ">
           <img src={about} alt="" className="rounded-md" />
         </div>
-        <div className="  absolute bottom-10 right-10 h-72 w-56 ">
+        <div className=" m-10  absolute inset-0  rounded-md h-72 w-56 ">
+          <img src={down} alt="" className="rounded-md" />
+        </div>
+        <div className="  absolute bottom-0 right-10 h-72 w-56 ">
           <img src={service} alt="" className="rounded-md" />
         </div>
       </div>
