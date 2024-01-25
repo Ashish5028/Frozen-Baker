b4f088f8275ddffc356c60d3b2ac56dad6993ee7
Abhishek232110 <abhi7376862392@gmail.com>
2024-01-25 22:19:35 +0530

    ass

--- src/service/component/imagePage.jsx
+++ src/service/component/imagePage.jsx
@@ -1,11 +1,10 @@
-import cake from "../../assets/baker.png";
 import service from "../../assets/service-2.jpg";
 import down from "../../assets/down.jpg";
 import about from "../../assets/about.jpg";
 export default function ImagePage() {
   return (
     <>
-      <div className=" relative h-full mx-10  ">
+      <div className=" relative h-screen mx-10  ">
         <div className=" absolute left-10 top-10 h-72 w-56 rounded-md ">
           <img src={down} alt="" className="rounded-md" />
         </div>
