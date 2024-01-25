8951745c3ede6008831cccf8c125126f9888c8e2
Ashish5028 <114331767+Ashish5028@users.noreply.github.com>
2024-01-25 18:55:15 +0530

    service

--- src/service/component/testApi.jsx
+++ src/service/component/testApi.jsx
@@ -1,32 +1,43 @@
 import { useState, useEffect } from "react";
+import { FaTshirt, FaCartPlus } from "react-icons/fa";
+import { BsCake2 } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
+
 import Card from "../../component/card";
 const TestApi = () => {
-  const [photos, setPhotos] = useState([]);
-  // useEffect(() => {
-  //   fetch("http://localhost:3001/api/get/image")
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setPhotos(data);
-  //     });
-  // }, []);
   return (
-    <div>
-      {/* {photos.map((photo) => (
-        <div className="float-left h-64 w-72 m-3 bg-cardColor rounded-md  transition ease-out delay-150 hover:duration-500  hover:shadow-xl hover:-translate-y-2 hover:bg-cardbgColor">
-          <img src={photo.imageUrl} className="w-72 h-52" />
-          <p>{photo.price}</p>
-          <p>{photo.name}</p>
+    <div className="flex  h-screen px-20">
+      <div className=" w-1/2 bg-cyan-300 pl-10">
+        <h1 className="text-bgColor">//OUR SERVICE</h1>
+        <br />
+        <p className="text-4xl">What Do We Offer For You ?</p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          <br />
+          exercitationem culpa debitis eligendi aliquid consectetur,
+          <br />
+          placeat in officia nemo hic?
+        </p>
+        <div className="flex justify-around ">
+          <FaTshirt className="h-10 w-10 bg-yellow-500 text-white rounded-full border p-2" />
+          <h1 className="">Quality Products</h1>
+          <BsCake2 className="h-10 w-10 bg-yellow-500 text-white rounded-full border p-2" />
+          <h1>Custom Products</h1>
         </div>
-      ))} */}
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-        exercitationem culpa debitis eligendi aliquid consectetur, numquam et
-        temporibus! Aspernatur nostrum non soluta consectetur unde enim placeat
-        in officia nemo hic?
-      </p>
+        <div className=" grid grid-cols-2 gap-4 place-content-around ">
+          <FaCartPlus className="h-10 w-10 bg-yellow-500 text-white rounded-full border p-2" />
+          <TbTruckDelivery className="h-10 w-10 bg-yellow-500 text-white rounded-full border p-2" />
+        </div>
+      </div>
+      <div className="w-1/2 bg-yellow-100">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe quidem
+          quae ipsum debitis facilis hic vel dignissimos veritatis voluptatem.
+          Odit optio vitae, veniam eius cumque corrupti dolore quidem magni
+          sapiente!
+        </p>
+      </div>
     </div>
   );
 };
